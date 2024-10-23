c59353a5646d9c2b58d753f131838de6a78afc4c
David Hodgson <dave@hodgsons.me.uk>
2024-10-23 12:40:49 +0100

    remove space

--- src/App.js
+++ src/App.js
@@ -50,8 +50,6 @@ function Board({xIsNext, squares, onPlay}) {
   );
 }
 
-
-
 export default function Game(){
   const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
